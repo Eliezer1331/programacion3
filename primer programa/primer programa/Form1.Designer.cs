869aaa0e0424e47c3f808403e7add59f0ba830df
Eliezer1331 <eliezer20002012gustavo4043@gmail.com>
2022-02-22 11:42:28 -0600

    primer programa
    programa de suma de dos numeros y mostrar mensaje en la pantallla

---
+++ primer programa/primer programa/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace primer_programa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.Numero2TextBox = new System.Windows.Forms.TextBox();
+            this.ResultadoTextBox = new System.Windows.Forms.TextBox();
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.MensajeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "numero 2:";
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(177, 84);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Numero1TextBox.TabIndex = 2;
+            // 
+            // Numero2TextBox
+            // 
+            this.Numero2TextBox.Location = new System.Drawing.Point(177, 129);
+            this.Numero2TextBox.Name = "Numero2TextBox";
+            this.Numero2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Numero2TextBox.TabIndex = 3;
+            // 
+            // ResultadoTextBox
+            // 
+            this.ResultadoTextBox.Location = new System.Drawing.Point(177, 199);
+            this.ResultadoTextBox.Name = "ResultadoTextBox";
+            this.ResultadoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ResultadoTextBox.TabIndex = 4;
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Location = new System.Drawing.Point(402, 84);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(75, 23);
+            this.EjecutarButton.TabIndex = 5;
+            this.EjecutarButton.Text = "ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // MensajeButton
+            // 
+            this.MensajeButton.Location = new System.Drawing.Point(402, 136);
+            this.MensajeButton.Name = "MensajeButton";
+            this.MensajeButton.Size = new System.Drawing.Size(75, 23);
+            this.MensajeButton.TabIndex = 6;
+            this.MensajeButton.Text = "Mostrar mensaje";
+            this.MensajeButton.UseVisualStyleBackColor = true;
+            this.MensajeButton.Click += new System.EventHandler(this.MensajeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 351);
+            this.Controls.Add(this.MensajeButton);
+            this.Controls.Add(this.EjecutarButton);
+            this.Controls.Add(this.ResultadoTextBox);
+            this.Controls.Add(this.Numero2TextBox);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.TextBox Numero2TextBox;
+        private System.Windows.Forms.TextBox ResultadoTextBox;
+        private System.Windows.Forms.Button EjecutarButton;
+        private System.Windows.Forms.Button MensajeButton;
+    }
+}
+
---
+++ primer programa/primer programa/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace primer_programa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(Numero1TextBox.Text);
+            int num2 = Convert.ToInt32(Numero2TextBox.Text);
+
+            ResultadoTextBox.Text = Sumar(num1, num2).ToString();
+        }
+         
+        private int Sumar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+
+            return resultado;
+        }
+
+
+        private void MostrarMensaje()
+        {
+            MessageBox.Show("Este es un mensaje");
+        }
+
+        private void MensajeButton_Click(object sender, EventArgs e)
+        {
+            MostrarMensaje();
+        }
+    }
+}
